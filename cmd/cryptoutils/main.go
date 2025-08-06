c690e226e19ce36910a6671c3b53ee76c20c94f6
Sandi <43517564+sandibrrm@users.noreply.github.com>
2025-08-07 00:23:33 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/cryptoutils/main.go
@@ -0,0 +1,23 @@
+// cmd/cryptoutils/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "cryptoutils/internal/cryptoutils"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := cryptoutils.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
